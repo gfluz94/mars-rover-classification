17214adf49fa4957ecc9cd312cc42d2bbae4057e
gabriel <gfluz94@gmail.com>
2021-03-15 21:27:14 -0300

    update url to files

--- preprocessing.py
+++ preprocessing.py
@@ -6,19 +6,19 @@ import pandas as pd
 import numpy as np
 
 
-TRAIN_ZIP_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/train.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210314%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210314T174808Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=80345413f4a175614f71638c6d391e7b8b138ddf2e29e26bf051971e12135022"
+TRAIN_ZIP_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/train.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210316%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210316T002519Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=f31a9cf93bae561d6e27acff8f22dcb753890505c8c92108d9e289592e718073"
 TRAIN_ZIP_FILE_PATH = "./src/train.zip"
-TRAIN_LABELS_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/train.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210314%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210314T174808Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=8be7fd371905eeb62c717e8556f8375cbd2f3a26908f2188745ad93b6fa93d0c"
+TRAIN_LABELS_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/train.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210316%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210316T002519Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=bbcf4f5068c38c786945982340825e7c051f92431fb082a5fbe98d7afc229eb5"
 TRAIN_LABELS_FILE = "./src/train.csv"
 TRAIN_FINAL_PATH = "./dataset/train"
 
-VAL_ZIP_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/val.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210314%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210314T174808Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=f7f4c1bf48c0ad0c65874313f16f4f6a3202c5e2c68a3f7ccf83c22673e85677"
+VAL_ZIP_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/val.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210316%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210316T002519Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=586be8cc93ba00df5ce42778218ccd2cc66fef967134f8eebf633ab8a72925cd"
 VAL_ZIP_FILE_PATH = "./src/val.zip"
-VAL_LABELS_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/val.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210314%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210314T174808Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=6308899139a0b56c7c68d5b0eb4e873de80282baa88b2c00a9c517744b7435bf"
+VAL_LABELS_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/val.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210316%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210316T002519Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=ddea78120c425b946abe6fc1e84cac055b73d165ae32bfbc8a1f0105188f4733"
 VAL_LABELS_FILE = "./src/val.csv"
 VAL_FINAL_PATH = "./dataset/val"
 
-TEST_ZIP_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/test.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210314%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210314T174808Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=b9982fb8479136219648afc020d924cf9bdac8904bdcc7e3e0c9a84b7aff23a4"
+TEST_ZIP_URL = "https://aicrowd-practice-challenges.s3.us-west-002.backblazeb2.com/public/roverclassification/v0.1/test.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=002ae2491b744be0000000003%2F20210316%2Fus-west-002%2Fs3%2Faws4_request&X-Amz-Date=20210316T002519Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=2c8230bf3ef439e3b53813a2bd4f33b8b670814922b6ec9dcd18bf36a4a40266"
 TEST_ZIP_FILE_PATH = "./src/test.zip"
 TEST_LABELS_FILE = "./src/test.csv"
 TEST_FINAL_PATH = "./dataset/test"
